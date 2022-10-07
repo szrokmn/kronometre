1e9bffa56912a512eae37ed18039840a833338e0
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-07 23:30:02 +0300

    js finished

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,16 @@
 const sayac = document.querySelector("#sayac");
 const btnBasla = document.querySelector("#btn-basla");
+
+let sayi = -1;
+let intervalID;
+btnBasla.addEventListener("click", function(){
+      if(sayi==-1){
+      intervalID=setInterval(function(){
+      sayi +=1;  
+      sayac.textContent =sayi;
+},1000);
+}else
+{
+  clearInterval(intervalID);
+}
+});
